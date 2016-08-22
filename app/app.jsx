d64c975827559298bf569cafe57466056c5efa33
Hyungju Kwon <hyungju.kwon@edelman.com>
2016-08-22 17:00:13 -0400

    Added Foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,11 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load fiundation
+
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+$(document).foundation();
 
 ReactDOM.render (
   <Router history={hashHistory}>
