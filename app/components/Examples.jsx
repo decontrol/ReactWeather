651506a0ac43d2257dcbfe6b28db656d1350e2e8
Hyungju Kwon <hyungju.kwon@edelman.com>
2016-08-22 14:20:04 -0400

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,6 +8,12 @@ var React = require('react');
 //   }
 // });
 
-var Examples = (props) => <h3>Examples Component</h3>;
-
+var Examples = (props) => {
+    return (    
+     <div>
+        <h3>Examples Component</h3>
+        <p>Welcome to Examples page!</p>
+     </div>
+    )
+};
 module.exports = Examples;
